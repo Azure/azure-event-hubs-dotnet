f8fd3d3ed3265f4c2c9c670517b9940b5b2c4ec6
Sean Feldman <feldman.sean@gmail.com>
2016-11-10 15:41:12 -0700

    removing configureAwait from sample
    closing connection
    moving client outside of the SendMessagesToEventHub method

--- samples/SampleSender/Program.cs
+++ samples/SampleSender/Program.cs
@@ -10,6 +10,7 @@ namespace SampleSender
 
     public class Program
     {
+        private static EventHubClient eventHubClient;
         private const string EhConnectionString = "{Event Hubs connection string}";
         private const string EhEntityPath = "{Event Hub path/name}";
 
@@ -20,14 +21,6 @@ namespace SampleSender
 
         private static async Task MainAsync(string[] args)
         {
-            await SendMessagesToEventHub(100).ConfigureAwait(false);
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadLine();
-        }
-
-        // Creates an Event Hub client and sends 100 messages to the event hub.
-        private static async Task SendMessagesToEventHub(int numMessagesToSend)
-        {
             // Creates an EventHubsConnectionStringBuilder object from a the connection string, and sets the EntityPath.
             // Typically the connection string should have the Entity Path in it, but for the sake of this simple scenario
             // we are using the connection string from the namespace.
@@ -36,22 +29,33 @@ namespace SampleSender
                 EntityPath = EhEntityPath
             };
 
-            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
 
+            await SendMessagesToEventHub(100);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        // Creates an Event Hub client and sends 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
             for (var i = 0; i < numMessagesToSend; i++)
             {
                 try
                 {
                     var message = $"Message {i}";
                     Console.WriteLine($"Sending message: {message}");
-                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message))).ConfigureAwait(false);
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
                 }
                 catch (Exception exception)
                 {
                     Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
                 }
 
-                await Task.Delay(10).ConfigureAwait(false);
+                await Task.Delay(10);
             }
 
             Console.WriteLine($"{numMessagesToSend} messages sent.");
