c6237679b8c87453ddfd0668e3f5a92784ff06d1
jtaubensee <jotaub@microsoft.com>
2016-12-09 12:59:52 -0800

    Moving scripts to a single file

--- build/BuildSolution.ps1
+++
@@ -1,9 +0,0 @@
-dotnet restore
-    
-dotnet build src/Microsoft.Azure.EventHubs/project.json
-    
-dotnet build src/Microsoft.Azure.EventHubs.Processor/project.json
-    
-dotnet build test/Microsoft.Azure.EventHubs.UnitTests/project.json
-    
-dotnet build test/Microsoft.Azure.EventHubs.Processor.UnitTests/project.json
