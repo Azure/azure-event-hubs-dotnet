a7e58a130a85913b0ec588dad99b0e08dc8bd755
Serkant Karaca <serkar@microsoft.com>
2017-02-21 14:17:05 -0800

    EPH to deliver Enumerable.Empty instead of NULL when EventProcessorOptions.InvokeProcessorAfterReceiveTimeout is set

--- src/Microsoft.Azure.EventHubs.Processor/PartitionPump.cs
+++ src/Microsoft.Azure.EventHubs.Processor/PartitionPump.cs
@@ -123,10 +123,17 @@ namespace Microsoft.Azure.EventHubs.Processor
 
         protected async Task ProcessEventsAsync(IEnumerable<EventData> events)
         {
-            // Assumes that .NET Core client will call with null on receive timeout.
-            if (events == null && !this.Host.EventProcessorOptions.InvokeProcessorAfterReceiveTimeout)
+            if (events == null)
             {
-                return;
+                if (!this.Host.EventProcessorOptions.InvokeProcessorAfterReceiveTimeout)
+                {
+                    return;
+                }
+                else
+                {
+                    // Assumes that .NET Core client will call with empty EventData on receive timeout.
+                    events = Enumerable.Empty<EventData>();
+                }
             }
 
             // Synchronize to serialize calls to the processor.
