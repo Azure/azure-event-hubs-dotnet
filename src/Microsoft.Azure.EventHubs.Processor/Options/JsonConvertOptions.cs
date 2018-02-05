34046ee9cc62124c78f3d0c5eff0b5bdd7b91238
Bart Jan van Assen <bart@appmachine.com>
2018-02-05 09:34:12 +0100

    add settings for json convert

---
+++ src/Microsoft.Azure.EventHubs.Processor/Options/JsonConvertOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Microsoft.Azure.EventHubs.Processor.Options
+{
+    /// <summary>
+    /// JsonConvert Options
+    /// </summary>
+    public static class JsonConvertOptions
+    {
+        /// <summary>
+        /// Get the JsonConvert formatting options. so it's not conflicting / dependent upon the user's code.
+        /// </summary>
+        /// <returns></returns>
+        public static JsonSerializerSettings GetOptions()
+        {
+            return new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+        }
+    }
+}
