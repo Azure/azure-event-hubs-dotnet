bf973e0594bd57af2e6b6ab5494bed12233cecc0
Serkant Karaca <serkar@microsoft.com>
2018-04-25 14:03:19 -0700

    Move EPH negative test to its own class.

--- test/Microsoft.Azure.EventHubs.Tests/Processor/ProcessorTestBase.cs
+++ test/Microsoft.Azure.EventHubs.Tests/Processor/ProcessorTestBase.cs
@@ -862,6 +862,151 @@ namespace Microsoft.Azure.EventHubs.Tests.Processor
             Assert.True(runResult.NumberOfFailures == 0, $"RunResult returned with {runResult.NumberOfFailures} failures!");
         }
 
+
+        /// <summary>
+        /// While processing events one event causes a failure. Host should be able to recover any error.
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        [DisplayTestMethodName]
+        async Task HostShouldRecoverWhenProcessEventsAsyncThrows()
+        {
+            var lastReceivedAt = DateTime.Now;
+            var lastReceivedAtLock = new object();
+            var poisonMessageReceived = false;
+            var poisonMessageProperty = "poison";
+            var processorFactory = new TestEventProcessorFactory();
+            var receivedEventCounts = new ConcurrentDictionary<string, int>();
+
+            var eventProcessorHost = new EventProcessorHost(
+                null,
+                PartitionReceiver.DefaultConsumerGroupName,
+                TestUtility.EventHubsConnectionString,
+                TestUtility.StorageConnectionString,
+                Guid.NewGuid().ToString());
+
+            processorFactory.OnCreateProcessor += (f, createArgs) =>
+            {
+                var processor = createArgs.Item2;
+                string partitionId = createArgs.Item1.PartitionId;
+                string hostName = createArgs.Item1.Owner;
+                string consumerGroupName = createArgs.Item1.ConsumerGroupName;
+                processor.OnOpen += (_, partitionContext) => TestUtility.Log($"{hostName} > {consumerGroupName} > Partition {partitionId} TestEventProcessor opened");
+                processor.OnClose += (_, closeArgs) => TestUtility.Log($"{hostName} > {consumerGroupName} > Partition {partitionId} TestEventProcessor closing: {closeArgs.Item2}");
+                processor.OnProcessError += (_, errorArgs) =>
+                {
+                    TestUtility.Log($"{hostName} > {consumerGroupName} > Partition {partitionId} TestEventProcessor process error {errorArgs.Item2.Message}");
+
+                    // Throw once more here depending on where we are at exception sequence.
+                    if (errorArgs.Item2.Message.Contains("ExceptionSequence1"))
+                    {
+                        throw new Exception("ExceptionSequence2");
+                    }
+                };
+                processor.OnProcessEvents += (_, eventsArgs) =>
+                {
+                    int eventCount = eventsArgs.Item2.events != null ? eventsArgs.Item2.events.Count() : 0;
+                    TestUtility.Log($"{hostName} > {consumerGroupName} > Partition {partitionId} TestEventProcessor processing {eventCount} event(s)");
+                    if (eventCount > 0)
+                    {
+                        lock (lastReceivedAtLock)
+                        {
+                            lastReceivedAt = DateTime.Now;
+                        }
+
+                        foreach (var e in eventsArgs.Item2.events)
+                        {
+                            // If this is poisoned event then throw.
+                            if (!poisonMessageReceived && e.Properties.ContainsKey(poisonMessageProperty))
+                            {
+                                poisonMessageReceived = true;
+                                TestUtility.Log($"Received poisoned message from partition {partitionId}");
+                                throw new Exception("ExceptionSequence1");
+                            }
+
+                            // Track received events so we can validate at the end.
+                            if (!receivedEventCounts.ContainsKey(partitionId))
+                            {
+                                receivedEventCounts[partitionId] = 0;
+                            }
+
+                            receivedEventCounts[partitionId]++;
+                        }
+                    }
+                };
+            };
+
+            try
+            {
+                TestUtility.Log("Registering processorFactory...");
+                var epo = new EventProcessorOptions()
+                {
+                    MaxBatchSize = 100,
+                    InitialOffsetProvider = pId => EventPosition.FromEnqueuedTime(DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(60)))
+                };
+                await eventProcessorHost.RegisterEventProcessorFactoryAsync(processorFactory, epo);
+
+                TestUtility.Log("Waiting for partition ownership to settle...");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                var client = EventHubClient.CreateFromConnectionString(TestUtility.EventHubsConnectionString);
+
+                // Send first set of messages.
+                TestUtility.Log("Sending an event to each partition as the first set of messages.");
+                var sendTasks = new List<Task>();
+                foreach (var partitionId in PartitionIds)
+                {
+                    sendTasks.Add(TestUtility.SendToPartitionAsync(client, partitionId, $"{partitionId} event."));
+                }
+                await Task.WhenAll(sendTasks);
+
+                // Now send 1 poisoned message. This will fail one of the partition pumps.
+                TestUtility.Log($"Sending a poison event to partition {PartitionIds.First()}");
+                var pSender = client.CreatePartitionSender(PartitionIds.First());
+                var ed = new EventData(Encoding.UTF8.GetBytes("This is poison message"));
+                ed.Properties[poisonMessageProperty] = true;
+                await pSender.SendAsync(ed);
+
+                // Wait sometime. The host should fail and then recever during this time.
+                await Task.Delay(30000);
+
+                // Send second set of messages.
+                TestUtility.Log("Sending an event to each partition as the second set of messages.");
+                sendTasks.Clear();
+                foreach (var partitionId in PartitionIds)
+                {
+                    sendTasks.Add(TestUtility.SendToPartitionAsync(client, partitionId, $"{partitionId} event."));
+                }
+                await Task.WhenAll(sendTasks);
+
+                TestUtility.Log("Waiting until hosts are idle, i.e. no more messages to receive.");
+                while (lastReceivedAt > DateTime.Now.AddSeconds(-60))
+                {
+                    await Task.Delay(1000);
+                }
+
+                TestUtility.Log("Verifying poison message was received");
+                Assert.True(poisonMessageReceived, "Didn't receive poison message!");
+
+                TestUtility.Log("Verifying received events by each partition");
+                foreach (var partitionId in PartitionIds)
+                {
+                    if (!receivedEventCounts.ContainsKey(partitionId))
+                    {
+                        throw new Exception($"Partition {partitionId} didn't receive any messages!");
+                    }
+
+                    var receivedEventCount = receivedEventCounts[partitionId];
+                    Assert.True(receivedEventCount >= 2, $"Partition {partitionId} received {receivedEventCount} where as at least 2 expected!");
+                }
+            }
+            finally
+            {
+                TestUtility.Log("Calling UnregisterEventProcessorAsync.");
+                await eventProcessorHost.UnregisterEventProcessorAsync();
+            }
+        }
+
         async Task<Dictionary<string, Tuple<string, DateTime>>> DiscoverEndOfStream()
         {
             var ehClient = EventHubClient.CreateFromConnectionString(TestUtility.EventHubsConnectionString);
