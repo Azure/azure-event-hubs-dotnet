c01b375347e623180cf772ab7a1a91e642264308
David Revoledo <davidrevoledo@d-genix.com>
2018-11-15 12:31:45 -0300

    Fix Namespace

--- src/Microsoft.Azure.EventHubs/EventDataDiagnosticExtensions.cs
+++ src/Microsoft.Azure.EventHubs/EventDataDiagnosticExtensions.cs
@@ -1,13 +1,12 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.Azure.EventHubs.Primitives;
-
 namespace Microsoft.Azure.EventHubs
 {
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using Microsoft.Azure.EventHubs.Primitives;
 
     /// <summary>
     /// Diagnostic extension methods for <see cref="EventData"/>.
