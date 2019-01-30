264c1ef92f6b8963472dfa5b763cf87f5b1b50b1
JamesBirdsall <jbird@microsoft.com>
2019-01-29 17:48:22 -0800

    Start of test cases

---
+++ test/Microsoft.Azure.EventHubs.Tests/ServiceFabricProcessor/SFPtestbase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.Azure.EventHubs.Tests.ServiceFabricProcessor
+{
+    public class SFPtestbase
+    {
+        public SFPtestbase()
+        {
+        }
+
+        [Fact]
+        [DisplayTestMethodName]
+        void SmokeTest()
+        {
+            TestUtility.Log("Hello world");
+        }
+    }
+}
