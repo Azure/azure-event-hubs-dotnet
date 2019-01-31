949b72e89a10bb897cf7a96c377f4b6b9a623983
JamesBirdsall <jbird@microsoft.com>
2019-01-30 17:38:54 -0800

    Fixes to Service Fabric mocks

--- test/Microsoft.Azure.EventHubs.Tests/ServiceFabricProcessor/MockPartitionLister.cs
+++ test/Microsoft.Azure.EventHubs.Tests/ServiceFabricProcessor/MockPartitionLister.cs
@@ -6,28 +6,32 @@ using System.Fabric.Query;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.EventHubs.ServiceFabricProcessor;
+using Xunit;
 
 namespace Microsoft.Azure.EventHubs.Tests.ServiceFabricProcessor
 {
     class MockPartitionLister : IFabricPartitionLister
     {
-        private readonly int count;
-        private readonly int ordinal;
+        private readonly int partitionCount;
+        private readonly int partitionOrdinal;
 
         public MockPartitionLister(int count, int ordinal)
         {
-            this.count = count;
-            this.ordinal = ordinal;
+            Assert.True(count > 0, $"Count must be at least 1, not {count}");
+            Assert.True(ordinal >= 0, $"Ordinal must be at least 0, not {ordinal}");
+            Assert.True(ordinal < count, $"Ordinal {ordinal} too large for count {count}");
+            this.partitionCount = count;
+            this.partitionOrdinal = ordinal;
         }
 
         public Task<int> GetServiceFabricPartitionCount(Uri serviceFabricServiceName)
         {
-            return Task.FromResult<int>(this.count);
+            return Task.FromResult<int>(this.partitionCount);
         }
 
         public Task<int> GetServiceFabricPartitionOrdinal(Guid serviceFabricPartitionId)
         {
-            return Task.FromResult<int>(this.ordinal);
+            return Task.FromResult<int>(this.partitionOrdinal);
         }
     }
 }
