321f18844876acc0c66cf6a98d3d35fb9a203efc
JamesBirdsall <jbird@microsoft.com>
2019-02-14 10:39:17 -0800

    ServiceFabricProcessor preview (#262)
    This is the code that built and released as preview version 0.5.2 https://www.nuget.org/packages/Microsoft.Azure.EventHubs.ServiceFabricProcessor/0.5.2
    
    At the time it couldn't be merged with dev due to test issues from unrelated work, so we did the release from the SFprocessor branch. Those issues have been resolved, and we expect that future preview releases will come from dev branch.

---
+++ src/Microsoft.Azure.EventHubs.ServiceFabricProcessor/EventHubMocks.cs
@@ -0,0 +1,209 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.using System;
+
+namespace Microsoft.Azure.EventHubs.ServiceFabricProcessor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Mocks for the underlying event hub client. Using these instead of the regular wrappers allows unit testing without an event hub.
+    /// By default, EventProcessorService.EventHubClientFactory is a EventHubWrappers.EventHubClientFactory.
+    /// To use the mocks, change it to a EventHubMocks.EventHubClientFactoryMock.
+    /// </summary>
+    public class EventHubMocks
+    {
+        /// <summary>
+        /// Mock of an Event Hub partition receiver.
+        /// </summary>
+        public class PartitionReceiverMock : EventHubWrappers.IPartitionReceiver
+        {
+            private readonly string partitionId;
+            private long sequenceNumber;
+            private IPartitionReceiveHandler outerHandler;
+            private bool invokeWhenNoEvents;
+            private readonly CancellationToken token;
+
+            /// <summary>
+            /// Construct the partition receiver mock.
+            /// </summary>
+            /// <param name="partitionId"></param>
+            /// <param name="sequenceNumber"></param>
+            /// <param name="token"></param>
+            public PartitionReceiverMock(string partitionId, long sequenceNumber, CancellationToken token)
+            {
+                this.partitionId = partitionId;
+                this.sequenceNumber = sequenceNumber;
+                this.token = token;
+            }
+
+            /// <summary>
+            /// Receive mock events.
+            /// </summary>
+            /// <param name="maxEventCount"></param>
+            /// <param name="waitTime"></param>
+            /// <returns></returns>
+            public Task<IEnumerable<EventData>> ReceiveAsync(int maxEventCount, TimeSpan waitTime)
+            {
+                List<EventData> events = new List<EventData>();
+                for (int i = 0; i < maxEventCount; i++)
+                {
+                    this.sequenceNumber++;
+                    byte[] body = new byte[] { 0x4D, 0x4F, 0x43, 0x4B, 0x42, 0x4F, 0x44, 0x59 }; // M O C K B O D Y
+                    EventData e = new EventData(body);
+                    // TODO -- need a way to set the system properties of the EventData
+                    //e.ForceSystemProperties(new EventData.SystemPropertiesCollection(this.sequenceNumber, DateTime.UtcNow, (this.sequenceNumber * 100).ToString(), ""));
+                    e.Properties.Add("userkey", "uservalue");
+                    events.Add(e);
+                }
+                Thread.Sleep(5000);
+                EventProcessorEventSource.Current.Message($"MOCK ReceiveAsync returning {maxEventCount} events for partition {this.partitionId} ending at {this.sequenceNumber}");
+                return Task.FromResult<IEnumerable<EventData>>(events);
+            }
+
+            /// <summary>
+            /// Set a mock receive handler.
+            /// </summary>
+            /// <param name="receiveHandler"></param>
+            /// <param name="invokeWhenNoEvents"></param>
+            public void SetReceiveHandler(IPartitionReceiveHandler receiveHandler, bool invokeWhenNoEvents = false)
+            {
+                EventProcessorEventSource.Current.Message("MOCK IPartitionReceiver.SetReceiveHandler");
+                this.outerHandler = receiveHandler;
+                this.invokeWhenNoEvents = invokeWhenNoEvents; // TODO mock does not emulate receive timeouts
+                if (this.outerHandler != null)
+                {
+                    Task.Run(() => GenerateMessages());
+                }
+            }
+
+            /// <summary>
+            /// Close the mock receiver.
+            /// </summary>
+            /// <returns></returns>
+            public Task CloseAsync()
+            {
+                EventProcessorEventSource.Current.Message("MOCK IPartitionReceiver.CloseAsync");
+                return Task.CompletedTask;
+            }
+
+            private async void GenerateMessages()
+            {
+                while ((!this.token.IsCancellationRequested) && (this.outerHandler != null))
+                {
+                    EventProcessorEventSource.Current.Message("MOCK Generating messages and sending to handler");
+                    IEnumerable<EventData> events = ReceiveAsync(10, TimeSpan.FromSeconds(10.0)).Result; // TODO get count from somewhere real
+                    IPartitionReceiveHandler capturedHandler = this.outerHandler;
+                    if (capturedHandler != null)
+                    {
+                        await capturedHandler.ProcessEventsAsync(events);
+                    }
+                }
+                EventProcessorEventSource.Current.Message("MOCK Message generation ending");
+            }
+        }
+
+        /// <summary>
+        /// Mock of EventHubClient class.
+        /// </summary>
+        public class EventHubClientMock : EventHubWrappers.IEventHubClient
+        {
+            private readonly int partitionCount;
+            private readonly EventHubsConnectionStringBuilder csb;
+            private CancellationToken token = new CancellationToken();
+
+            /// <summary>
+            /// Construct the mock.
+            /// </summary>
+            /// <param name="partitionCount"></param>
+            /// <param name="csb"></param>
+            public EventHubClientMock(int partitionCount, EventHubsConnectionStringBuilder csb)
+            {
+                this.partitionCount = partitionCount;
+                this.csb = csb;
+            }
+
+            internal void SetCancellationToken(CancellationToken t)
+            {
+                this.token = t;
+            }
+
+            /// <summary>
+            /// Get runtime info of the fake event hub.
+            /// </summary>
+            /// <returns></returns>
+            public Task<EventHubRuntimeInformation> GetRuntimeInformationAsync()
+            {
+                EventHubRuntimeInformation ehri = new EventHubRuntimeInformation();
+                ehri.PartitionCount = this.partitionCount;
+                ehri.PartitionIds = new string[this.partitionCount];
+                for (int i = 0; i < this.partitionCount; i++)
+                {
+                    ehri.PartitionIds[i] = i.ToString();
+                }
+                ehri.Path = csb.EntityPath;
+                EventProcessorEventSource.Current.Message($"MOCK GetRuntimeInformationAsync for {ehri.Path}");
+                return Task.FromResult<EventHubRuntimeInformation>(ehri);
+            }
+
+            /// <summary>
+            /// Create a mock receiver on the fake event hub.
+            /// </summary>
+            /// <param name="consumerGroupName"></param>
+            /// <param name="partitionId"></param>
+            /// <param name="eventPosition"></param>
+            /// <param name="offset"></param>
+            /// <param name="epoch"></param>
+            /// <param name="receiverOptions"></param>
+            /// <returns></returns>
+            public EventHubWrappers.IPartitionReceiver CreateEpochReceiver(string consumerGroupName, string partitionId, EventPosition eventPosition, string offset, long epoch, ReceiverOptions receiverOptions)
+            {
+                EventProcessorEventSource.Current.Message($"MOCK CreateEpochReceiver(CG {consumerGroupName}, part {partitionId}, offset {offset} epoch {epoch})");
+                // TODO implement epoch semantics
+                long startSeq = (offset != null) ? (long.Parse(offset) / 100L) : 0L;
+                return new PartitionReceiverMock(partitionId, startSeq, this.token);
+            }
+
+            /// <summary>
+            /// Close the mock EventHubClient.
+            /// </summary>
+            /// <returns></returns>
+            public Task CloseAsync()
+            {
+                EventProcessorEventSource.Current.Message("MOCK IEventHubClient.CloseAsync");
+                return Task.CompletedTask;
+            }
+        }
+
+        /// <summary>
+        /// An EventHubClient factory which dispenses mocks.
+        /// </summary>
+        public class EventHubClientFactoryMock : EventHubWrappers.IEventHubClientFactory
+        {
+            private readonly int partitionCount;
+
+            /// <summary>
+            /// Construct the mock factory.
+            /// </summary>
+            /// <param name="partitionCount"></param>
+            public EventHubClientFactoryMock(int partitionCount)
+            {
+                this.partitionCount = partitionCount;
+            }
+
+            /// <summary>
+            /// Dispense a mock instance operating on a fake event hub with name taken from the connection string.
+            /// </summary>
+            /// <param name="connectionString"></param>
+            /// <returns></returns>
+            public EventHubWrappers.IEventHubClient CreateFromConnectionString(string connectionString)
+            {
+                throw new NotImplementedException("Need a change to EventData before mocks can be supported");
+                //EventProcessorEventSource.Current.Message($"MOCK Creating IEventHubClient {connectionString} with {this.partitionCount} partitions");
+                //return new EventHubClientMock(this.partitionCount, new EventHubsConnectionStringBuilder(connectionString));
+            }
+        }
+    }
+}
