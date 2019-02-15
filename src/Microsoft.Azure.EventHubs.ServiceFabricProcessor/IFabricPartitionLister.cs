1840979f1318930d59a5715a2d493fae101f82ca
JamesBirdsall <jbird@microsoft.com>
2019-02-15 13:13:30 -0800

    Add copyright, move usings, remove unused

--- src/Microsoft.Azure.EventHubs.ServiceFabricProcessor/IFabricPartitionLister.cs
+++ src/Microsoft.Azure.EventHubs.ServiceFabricProcessor/IFabricPartitionLister.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Fabric.Query;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.using System;
 
 namespace Microsoft.Azure.EventHubs.ServiceFabricProcessor
 {
+    using System;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// 
     /// </summary>
